5f7f98a6db4321b28a264b9cddb4de60094c7e9d
“Rachel-Kisner” <rachel8508773@gmail.com>
2025-06-10 19:13:10 +0300

    login + header

---
+++ React/playSync/src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Header} from "./Header";
+import {Container,Box} from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+interface LayoutProps {
+    onNavigate: (page:string) => void;
+    children?: React.ReactNode;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ onNavigate }) => {
+  return (
+    <Box sx={{ minHeight: '100vh', backgroundColor: '#f4f6f8' }}>
+      <Header onNavigate={onNavigate} />
+      <Container sx={{ mt: 3, mb: 3 }}>
+        <Outlet /> {/* כאן יופיעו כל הדפים הפנימיים */}
+      </Container>
+    </Box>
+  );
+};

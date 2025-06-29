98577a5765dbb072fccbca815bd64a6f9d1edff4
“Rachel-Kisner” <rachel8508773@gmail.com>
2025-06-29 12:37:31 +0300

    add beatApi in python

--- Api/PlaySync/PlaySync/Controllers/SongController.cs
+++ Api/PlaySync/PlaySync/Controllers/SongController.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Design;
-using BL;
+﻿using BL;
 using DL.Entities;
-using Microsoft.EntityFrameworkCore;
-using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 
 
@@ -29,11 +27,12 @@ namespace PlaySyncApi.Controllers
                 var songs = await _songService.GetAllSongsAsync();
                 return Ok(songs);
             }
-            catch (Exception ex) { 
+            catch (Exception ex)
+            {
                 return BadRequest(ex.Message);
             }
         }
-        
+
         [HttpGet("{songId}")]
         public async Task<IActionResult> GetSongBySongId(int songId)
         {
@@ -46,7 +45,8 @@ namespace PlaySyncApi.Controllers
                 }
                 return Ok(song);
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 return BadRequest(ex.Message);
             }
         }
@@ -89,7 +89,7 @@ namespace PlaySyncApi.Controllers
             try
             {
                 var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-          
+
                 var song = await _songService.UploadSongAsync(request.File, request.Title, request.Artist, request.Genre, (bool)request.Favorite, userId);
                 return Ok(song);
             }
@@ -98,7 +98,7 @@ namespace PlaySyncApi.Controllers
         }
 
         [HttpPut("{songId}")]
-        [Authorize(Policy = "OwnerOrAdmin" )]
+        [Authorize(Policy = "OwnerOrAdmin")]
 
         public async Task<IActionResult> UpdateSong(int songId, [FromBody] SongRequestDto dto)
         {
@@ -107,7 +107,7 @@ namespace PlaySyncApi.Controllers
 
                 var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
                 var isAdmin = User.FindFirst(ClaimTypes.Role)?.Value == nameof(Role.Admin);
-                var song = await _songService.UpdateSongAsync(songId, dto,  userId,  isAdmin);
+                var song = await _songService.UpdateSongAsync(songId, dto, userId, isAdmin);
                 return song != null ? Ok(song) : NotFound();
             }
             //catch (UnauthorizedAccessException)
@@ -128,12 +128,14 @@ namespace PlaySyncApi.Controllers
         [Authorize(Policy = "OwnerOrAdmin")]
         public async Task<IActionResult> ToggleFavorite(int songId)
         {
-            try {
+            try
+            {
                 await _songService.ToggleFavoriteAsync(songId);
-                return Ok(new {message = "Favorite status toggled successfully."});
+                return Ok(new { message = "Favorite status toggled successfully." });
             }
-            catch (Exception ex) {
-                return BadRequest(new{message= ex.Message });
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
             }
         }
 
@@ -146,7 +148,7 @@ namespace PlaySyncApi.Controllers
                 var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
                 var role = User.FindFirstValue(ClaimTypes.Role);
                 bool isAdmin = role == "Admin";
-                 await _songService.DeleteSongAsync(songId, userId, isAdmin);
+                await _songService.DeleteSongAsync(songId, userId, isAdmin);
                 return NoContent(); ;
             }
             catch (UnauthorizedAccessException)
@@ -155,7 +157,7 @@ namespace PlaySyncApi.Controllers
             }
             catch (ArgumentException)
             {
-                return NotFound(); 
+                return NotFound();
             }
             catch (Exception ex)
             {
@@ -163,14 +165,46 @@ namespace PlaySyncApi.Controllers
             }
         }
 
+        [HttpPost("{songId}/ringtone-ai")]
+        [Authorize]
+        public async Task<IActionResult> GenerateRingtoneByAI(int songId)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 
-       
+            var song = await _songService.GetSongBySongIdAsync(songId);
+            if (song == null)
+                return NotFound("Song not found");
 
+            if (song.UserId != userId)
+                return Forbid("You arn't the woner of this song ");
+            if (string.IsNullOrEmpty(song.CloudinaryUrl))
+                return BadRequest("Song doesn't have a Cloudinary URL");
+
+            using var httpClient = new HttpClient();
+            using var songStream = await httpClient.GetStreamAsync(song.CloudinaryUrl);
+
+            using var form = new MultipartFormDataContent();
+            var streamContent = new StreamContent(songStream);
+            streamContent.Headers.ContentType=new MediaTypeHeaderValue("audio/wav");
+            form.Add(streamContent, "file", "song.wav");
+
+            ////don't forget to change!! -after spreading in render!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            var response = await httpClient.PostAsync("http://localhost:5001/analyze", form);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                return StatusCode((int)response.StatusCode, error);
+            }
+
+            var ringtoneBytes = await response.Content.ReadAsByteArrayAsync();
+
+            return File(ringtoneBytes, "audio/wav", "ringtone.wav");
+        }
 
 
     }
 }
-
 
 
 
